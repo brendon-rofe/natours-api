81aa54b3b5bf4b32d2c2bb6cc3813c49b54fa5e5
Bravo1B9 <brofe93@gmail.com>
2023-04-20 15:09:38 +0000

    Update

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello from the server!');
+  res.status(200).json({ msg: 'Hello from the server!' });
 });
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`App running on port: ${port}`);
+  console.log(`App running on port: ${port}...`);
 });
