bf0a39342973c9e41f0b6e11018e1780e98993c1
Brendon Rofe <b1@xgmi.com>
2023-04-21 06:51:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ app.get('/', (req, res) => {
   res.status(200).json({ msg: 'Index route' });
 });
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 app.get('/api/v1/tours', (req, res) => {
-  res.status(200).json({ success: 'success', data: { tours: tours } });
+  res.status(200).json({ success: 'success', data: { tours } });
 });
 
 const port = 3000;
