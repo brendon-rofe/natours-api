37b7511c43dc8c27d0be89489e88149aed05ac6c
Bravo1B9 <brofe93@gmail.com>
2023-04-21 04:19:11 +0000

    Update

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).json({ msg: 'Hello from the server!' });
+  res.status(200).json({ msg: 'Index route' });
+});
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({ msg: 'This returns all the tours' });
 });
 
 const port = 3000;
