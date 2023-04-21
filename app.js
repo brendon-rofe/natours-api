405a83734b96d1f5a02610a28035a9a5787b552d
Brendon Rofe <b1@xgmi.com>
2023-04-21 07:09:11 +0200

    Initial create tour functionality

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.status(200).json({ msg: 'Index route' });
 });
@@ -13,6 +15,11 @@ const tours = JSON.parse(
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({ success: 'success', results: tours.length, data: { tours } });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.send('Done');
 });
 
 const port = 3000;
