c807afb458774f15a209d3d8cb332e141ba41621
Brendon Rofe <b1@xgmi.com>
2023-04-21 07:24:21 +0200

    Implement adding a new tour functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const express = require('express');
+const { error } = require('console');
 
 const app = express();
 
@@ -18,8 +19,19 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 app.post('/api/v1/tours', (req, res) => {
-  console.log(req.body);
-  res.send('Done');
+  const newId = tours[tours.length - 1].id + 1;
+
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    res.status(201).json({
+      status: 'success',
+      data:  {
+        tour: newTour
+      }
+    });
+  });
 });
 
 const port = 3000;
