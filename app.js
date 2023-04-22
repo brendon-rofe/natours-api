e8e8889e39ff96b712d417fa822225ccfbe65a2f
Brendon Rofe <b1@xgmi.com>
2023-04-22 15:32:13 +0200

    Remove the old routes

--- app.js
+++ app.js
@@ -67,11 +67,6 @@ const deleteTour = (req, res) => {
 app.get('/', (req, res) => {
   res.status(200).json({ msg: 'Index route' });
 });
-app.get('/api/v1/tours', getAllTours);
-app.get('/api/v1/tours/:id', getTour);
-app.post('/api/v1/tours', createTour);
-app.patch('/api/v1/tours/:id', updateTour);
-app.delete('/api/v1/tours/:id', deleteTour);
 
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 app
