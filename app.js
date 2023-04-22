eacce2b0c1e7968e8d1ef819e4bee54e89ab5429
Brendon Rofe <b1@xgmi.com>
2023-04-22 14:51:45 +0200

    Implement initial patch functionality

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get('/api/v1/tours/:id', (req, res) => {
 
   if(id >= tours.length) {
     return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
-  }
+  };
 
   const tour = tours.find(t => t.id === id);
   res.status(200).json({ status: 'success', data: { tour: tour } });
@@ -44,6 +44,14 @@ app.post('/api/v1/tours', (req, res) => {
       }
     });
   });
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if(req.params.id * 1 >= tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  };
+
+  res.status(200).json({ status: 'success', data: { tour: '<Updated tour here...>' } });
 });
 
 const port = 3000;
